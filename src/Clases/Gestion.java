24987edd0e4fcfb4d77bf85c09a74af57e08c703
apala <apala@10.160.49.46>
2023-10-24 13:35:02 +0200

    Clase Gestion en proceso

--- src/Clases/Gestion.java
+++ src/Clases/Gestion.java
@@ -7,7 +7,7 @@ public class Gestion {
 	//Atributos
 	private static int numTurno = 1;
 	protected int Turno;
-	protected ArrayList<Jugador> jugadores = new ArrayList<Jugador>(); //Va a tener todos los jugadores de la partida
+	protected ArrayList<Jugador> jugadores = new ArrayList<Jugador>(); //Va a tener todos los jugadores de la partida.
 	protected int movimiento; //Esto va a ser la suma del resultado de los dados que te han tocado al tirarlos en tu turno.
 	
 	protected Contenedor datosPartida;
