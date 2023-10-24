194c7e0d2cdebcbe14a36e471bc294cd2c050b49
apala <apala@10.160.49.46>
2023-10-24 13:19:19 +0200

    Clase Jugador

--- src/Clases/Jugador.java
+++ src/Clases/Jugador.java
@@ -2,17 +2,85 @@ package Clases;
 
 import java.util.*;
 enum OpcionesLista{SI, NO, REGULAR}
-public class Jugador {
-	private static int codigo;
-	protected ArrayList<Asesinato> cartas = new ArrayList<>();
-	protected Personaje personaje = null;
-	protected HashMap<Implicados, Asesinato>acusacion = new HashMap<>();
-	protected boolean acusacionFinal = false;
-	protected HashMap<Asesinato, OpcionesLista>lista = new HashMap<>();
+
+public class Jugador { //Al crear un nuevo jugador el jugador tiene que tener todos estos datos
+	
+	private Random r = new Random();
+	
+	private static int contador = 1;
+	protected int codigo;
+	protected ArrayList<Asesinato> cartas = new ArrayList<>(); //Cartas que le tocan
+	protected Personaje personaje = null; //El personaje elegido
+	protected HashMap<Implicados, Asesinato> acusacion = new HashMap<>(); //Para al acusar que salga un HashMap con las tres acusaciones
+	protected boolean acusacionFinal = false; // Para cuando quieras hacer tu acusación final
+	protected HashMap<Asesinato, OpcionesLista> lista = new HashMap<>(); //Lista que se rellena con la información
+	
+	
+	public static int getContador() {
+		return contador;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public ArrayList<Asesinato> getCartas() {
+		return cartas;
+	}
+
+	public void setCartas(ArrayList<Asesinato> cartas) {
+		this.cartas = cartas;
+	}
+
+	public Personaje getPersonaje() {
+		return personaje;
+	}
+
+	public void setPersonaje(Personaje personaje) {
+		this.personaje = personaje;
+	}
+
+	public HashMap<Implicados, Asesinato> getAcusacion() {
+		return acusacion;
+	}
+
+	public void setAcusacion(HashMap<Implicados, Asesinato> acusacion) {
+		this.acusacion = acusacion;
+	}
+
+	public boolean isAcusacionFinal() {
+		return acusacionFinal;
+	}
+
+	public void setAcusacionFinal(boolean acusacionFinal) {
+		this.acusacionFinal = acusacionFinal;
+	}
+	
+	public HashMap<Asesinato, OpcionesLista> getLista() {
+		return lista;
+	}
+
+
+	public void setLista(HashMap<Asesinato, OpcionesLista> lista) {
+		this.lista = lista;
+	}
+
+	public Jugador(Personaje personaje) {
+		this.codigo = contador;
+		this.contador ++;
+		this.personaje = personaje;
+		this.cartas = new ArrayList<Asesinato>();
+		this.acusacionFinal = false;
+		this.acusacion = new HashMap<Implicados, Asesinato>();
+		this.lista = new HashMap<Asesinato, OpcionesLista>();
+	}
+
 	
 	public Jugador(ArrayList<Asesinato> cartas, Personaje personaje, HashMap<Implicados, Asesinato> acusacion,
 			boolean acusacionFinal, HashMap<Asesinato, OpcionesLista> lista) {
 		super();
+		this.codigo = contador;
+		this.contador ++;
 		this.cartas = cartas;
 		this.personaje = personaje;
 		this.acusacion = acusacion;
