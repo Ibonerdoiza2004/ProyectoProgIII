c029f56e355d4da3ae2277eeca4ee5b0e37a6704
User <User@Pc>
2023-11-03 16:04:27 +0100

    Prueba Github 4

--- src/Clases/Gestion.java
+++ src/Clases/Gestion.java
@@ -129,7 +129,7 @@ public class Gestion {
 		return matrizMapa;
 	}
 	
-	//PruebaGithub3
+	//PruebaGithub4
 	
 	public ArrayList<ArrayList<Integer>> elevarMatriz(int potencia, ArrayList<ArrayList<Integer>>matriz) {
 		ArrayList<ArrayList<Integer>>resultado= new ArrayList<>(matriz);
