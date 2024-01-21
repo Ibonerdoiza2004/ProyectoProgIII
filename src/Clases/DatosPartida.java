163ac3af6ed48921f6bc773923a51b29fcb23bc2
apala <apala@AnderPalaEnvy>
2024-01-21 20:48:31 +0100

    Cargando y guardando datos partida

--- src/Clases/DatosPartida.java
+++ src/Clases/DatosPartida.java
@@ -1,14 +1,32 @@
 package Clases;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.tools.FileObject;
 
-public class DatosPartida {
+public class DatosPartida implements Serializable {
 	
+	private static final String nomFichero = "partidasGuardadas";
+			
+	//private DateTimeFormatter formateador = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	private ArrayList<DatosPartida> partidasGuardadas = new ArrayList<DatosPartida>();
+	
+	private String identificadorPartida; //Se va a identificar con la fecha en la que se guardó
 	private int numTurno;
 	private ArrayList<Jugador> jugadoresPartida;
 	private ArrayList<Asesinato> acusacion;
@@ -16,17 +34,110 @@ public class DatosPartida {
 	private JPanel siguientePanel;
 
 	public static void main(String[] args) {
-		DatosPartida dp = new DatosPartida();
+		DatosPartida dp = new DatosPartida(0, null, null, null, new JPanel());
+		dp.guardarPartida(new DatosPartida(3, null, null, null, new JPanel()));
 		dp.cargarPartidaJO();
 	}
 	
-	public DatosPartida() { //Aquí vienen los datos de la partida
+	public DatosPartida(int numturno, ArrayList<Jugador> jugadores, ArrayList<Asesinato> acus, HashMap<Asesinato, Jugador> cartasensenyadas, JPanel sigPanel) { //Aquí vienen los datos desde Gestión
+		numTurno = numturno;
+		jugadoresPartida = jugadores;
+		acusacion = acus;
+		cartasEnsenyadas = cartasensenyadas;
+		siguientePanel = sigPanel;
 		
+        //LocalDateTime fechaHoraActual = LocalDateTime.now();
+        String nombrePartida = JOptionPane.showInputDialog("Ingresa el nombre para guardar la partida:");
+        
+        //identificadorPartida = nombrePartida + " " + fechaHoraActual.format(formateador);
+        identificadorPartida = nombrePartida;
+	}
+	
+	public String getIdentificadorPartida() {
+		return identificadorPartida;
+	}
+
+	public void setIdentificadorPartida(String identificadorPartida) {
+		this.identificadorPartida = identificadorPartida;
+	}
+
+	public int getNumTurno() {
+		return numTurno;
+	}
+
+	public void setNumTurno(int numTurno) {
+		this.numTurno = numTurno;
+	}
+
+	public ArrayList<Jugador> getJugadoresPartida() {
+		return jugadoresPartida;
+	}
+
+	public void setJugadoresPartida(ArrayList<Jugador> jugadoresPartida) {
+		this.jugadoresPartida = jugadoresPartida;
+	}
+
+	public ArrayList<Asesinato> getAcusacion() {
+		return acusacion;
+	}
+
+	public void setAcusacion(ArrayList<Asesinato> acusacion) {
+		this.acusacion = acusacion;
+	}
+
+	public HashMap<Asesinato, Jugador> getCartasEnsenyadas() {
+		return cartasEnsenyadas;
+	}
+
+	public void setCartasEnsenyadas(HashMap<Asesinato, Jugador> cartasEnsenyadas) {
+		this.cartasEnsenyadas = cartasEnsenyadas;
+	}
+
+	public JPanel getSiguientePanel() {
+		return siguientePanel;
+	}
+
+	public void setSiguientePanel(JPanel siguientePanel) {
+		this.siguientePanel = siguientePanel;
+	}
+
+	private void guardarPartida(DatosPartida dp) {
+		File f = new File(nomFichero);
+		
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		
+		try {
+			fos = new FileOutputStream(f);
+			oos = new ObjectOutputStream(fos);
+			
+			oos.writeObject(dp);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void cargarPartidaJO() {
-        String[] opciones = {"Opción 1", "Opción 2", "Opción 3"};
-
+		
+		ArrayList<String> ids = new ArrayList<String>();
+		
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomFichero));
+			DatosPartida dp = (DatosPartida) ois.readObject();
+			while (dp != null) {
+				partidasGuardadas.add(dp);
+				ids.add(dp.getIdentificadorPartida());
+			}
+			
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		String[] opciones = new String[ids.size()];
+		for (int i = 0; i < ids.size(); i ++) {
+			opciones[i] = ids.get(i);
+		}
         JComboBox<String> comboBox = new JComboBox<>(opciones);
         
         int opcionSeleccionada = JOptionPane.showOptionDialog(
